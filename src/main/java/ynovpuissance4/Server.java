455c7f22dd1241ac11106a27bad69d2ef20fcd96
ARROUSSI Mehdi <mehdi.arroussi@ynov.com>
2022-11-07 05:27:57 +0100

    Server setup ( need to fix Listen in Client )

--- src/main/java/ynovpuissance4/Server.java
+++ src/main/java/ynovpuissance4/Server.java
@@ -1,11 +1,110 @@
 package ynovpuissance4;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Random;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+
+
 
 public class Server {
+
+    /*
+     * This method is the main Server part. It makes a server and wait for the players (as Client) to connect. Then it communicates with it and launch an online game.
+     */
+    
+    public static void LaunchServeur(int numberPlayer) {
+        ArrayList<SocketChannel> listClient = new ArrayList<SocketChannel>();
+        try {
+            ServerSocketChannel serverSocket  = ServerSocketChannel.open();
+            serverSocket.bind(new InetSocketAddress(4004)); ///Port 4004 as asked in the subject.
+            while(numberPlayer != listClient.size()) { 
+                SocketChannel clientSocket = serverSocket.accept();
+                System.out.println("A Player has connected ! Get Ready !");
+                listClient.add(clientSocket);
+                broadcast(Integer.toString(numberPlayer), clientSocket);
+            }
+        }catch (IOException e) {
+            System.err.println(e.toString());
+        }
+        Player playerPlay = PlayerRandomizer(numberPlayer); 
+        Grid grid = new Grid(numberPlayer);
+        Map<Player, SocketChannel> dictionary = new HashMap<Player, SocketChannel>();
+        for (int i = 0; i < listClient.size(); i++) {
+            Player player;
+            if (i==0) {player = Player.Player1;}else if (i==1) {player = Player.Player2;}else {player = Player.Player3;}
+            dictionary.put(player, listClient.get(i));
+        }
+        while(!Conditions.Win(grid.grille)&& !Conditions.Equal(grid.grille)) { 
+            for (SocketChannel socketChannel : listClient) {
+                if (socketChannel == dictionary.get(playerPlay)) { 
+                    switch (playerPlay) {
+                        case Player1:
+                            broadcast("Player X , It's your turn !",socketChannel);
+                            break;
+                        case Player2:
+                            broadcast("Player O , It's your turn !",socketChannel);
+                            break;
+                        default:
+                            broadcast("Player V , It's your turn !",socketChannel);
+                            break;
+                    }
+                }else {
+                    broadcast("Turn",socketChannel);
+                }
+            }
+            String Message = "";
+            try {
+                Message = Client.Listen(dictionary.get(playerPlay)); 
+            }catch (IOException e) {
+                System.err.println("A player has disconnected ...");
+            }
+            for (SocketChannel socketChannel : listClient) {
+                broadcast(Message, socketChannel); 
+            }
+            playerPlay = NextPlayer(numberPlayer, playerPlay);
+            if (Message != "") {
+                Turn(grid.grille,String.valueOf(Message.charAt(5)),String.valueOf(Message.charAt(7)));
+            }else {
+                break;
+            }
+        }
+    }
+
+    /**
+     * This Method is for the online game, it sends a message to the Client.
+     * @param message the message (type String) we wanna send to the Client.
+     */
+    public static void broadcast(String message, SocketChannel clientSocket){
+        try{
+            ByteBuffer bytes = ByteBuffer.wrap(message.getBytes("UTF-8"));
+        while(bytes.hasRemaining()){
+            clientSocket.write(bytes);
+        }
+        }catch(IOException e){
+            System.err.println(e.toString());
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    
     /**
      * @param player this is the playing player
      * @param index this is the index of the column where the token has been thrown
